29992a4cbabf7f26238e5f7f90cdafd50dda0b6f
KAPPA <devil871018@gmail.com>
2020-07-09 18:26:31 +0800

    0709 影片連結多項題目版面60%

---
+++ src/components/LessonMod/LessonVideo.vue
@@ -0,0 +1,87 @@
+<script>
+export default {
+    name: "LessonVideo",
+    components: {
+
+    },
+    props: {
+        url: {
+            type: String,
+            default: () => {
+                console.log('url not found');
+            }
+        }
+    },
+    data(){
+        return{
+            newUrl: ''
+        }
+    },
+    methods: {
+        setUrl(videoCode){
+            return `https://www.youtube.com/embed/${videoCode}`;
+        },
+        updateUrl(){
+            if(this.newUrl === ''){
+                alert('請貼入網址');
+            }else{
+                this.newUrl = this.newUrl.trim();
+                var start = this.newUrl.indexOf('v=');
+                var last = this.newUrl.indexOf('&');
+                if(start === -1){
+                    this.$Message['error']({
+                        background: true,
+                        content: '找不到影片！請確認網址是否正確'
+                    });
+                    this.newUrl = this.url;
+                }else{
+                    if(last === -1){
+                        this.newUrl = this.newUrl.slice(start + 2);
+                    }else{
+                        this.newUrl = this.newUrl.slice(start + 2, last);
+                    }
+                }
+                
+                this.$emit('newUrl', this.newUrl);
+                this.newUrl = '';
+            }
+        },
+        alert(){
+            alert('???');
+        }
+    }
+}
+</script>
+<template lang="pug">
+.LessonVideo
+    .urlset
+        Input(placeholder="更新Youtube網址" v-model='newUrl')
+        Button(type=`success` @click='updateUrl') 測試
+    iframe( :src="setUrl(this.url)" frameborder="0" allowfullscreen)
+    Button(type='error' @click='alert') 刪除
+</template>
+<style lang="scss" scoped>
+.LessonVideo{
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-items: center;
+    iframe {
+        width: 60%;
+        min-height: 400px;
+    }
+    .urlset{
+        display: flex;
+        align-items: center;
+        width: 50%;
+        margin-top: 1em;
+        margin-bottom: 1em;
+        Button{
+            margin-left: 10px;
+        }
+    }
+    Button{
+        margin: 5px;
+    }
+}
+</style>
--- src/views/backstage/LessonList.vue
+++ src/views/backstage/LessonList.vue
@@ -1,43 +1,36 @@
 <script>
+import LessonVideo from '@/components/LessonMod/LessonVideo.vue';
 export default {
     name: 'LessonList',
     components:{
-        
+        LessonVideo
     },
     data(){
         return {
+            firstOpen: '1',
             lessons: [
                 {
-                    lessonTitle: `課程標題`,
-                    lessonUrl: `qWSKxGBv3bE`,
-                    question: [
-                        {
-                            quesContent: `question content`,
-                            questions: [
-                                {
-                                    question1: `q1`
-                                },
-                                {
-                                    question2: `q2`
-                                },
-                                {
-                                    question3: `q3`
-                                },
-                                {
-                                    question4: `q4`
-                                },
-
-                            ]
-                        }
-                    ],
-                    answer: `q1`
+                    lessonTitle: `課程標題1`,
+                    lessonUrl: `ZPn7OsUZaug`,
+                    question: [ ]
+                },
+                {
+                    lessonTitle: `課程標題2`,
+                    lessonUrl: `ZPn7OsUZaug`,
+                    question: [ ]
+                },
+                {
+                    lessonTitle: `課程標題3`,
+                    lessonUrl: `ZPn7OsUZaug`,
+                    question: [ ]
                 }
             ]
         }
     },
     methods: {
-        setUrl(videoCode){
-            return `https://www.youtube.com/embed/${videoCode}`;
+        updateNewUrl(newUrl, index){
+            console.log(newUrl);
+            this.lessons[index].lessonUrl = newUrl;
         }
     }
 }
@@ -45,8 +38,15 @@ export default {
 <template lang="pug">
     #lessonlist
         .list
-            iframe( :src="setUrl(lessons[0].lessonUrl)" frameborder="0" allowfullscreen)
-            | {{ this.$route.params.classID }}
+            .btnCollect
+                Button(type='info') 新增
+                //- div {{ firstOpen }}
+                Select(v-model='firstOpen')
+                    Option(v-for='(lesson, index) in lessons' :value='index + 1') {{ index }} - {{ lesson.lessonTitle }}
+            Collapse(v-model='firstOpen' simple)
+                Panel(v-for='(lesson, index) in lessons' :name='String(index + 1)') {{ lesson.lessonTitle }}
+                    LessonVideo(slot='content' :url='lesson.lessonUrl' @newUrl='updateNewUrl($event, index)')
+
 
 </template>
 <style lang="scss" scoped>
@@ -58,5 +58,10 @@ export default {
 .list{
     width: 80%;
     background-color: white;
+    .btnCollect{
+        display: flex;
+        
+    }
 }
+
 </style>
